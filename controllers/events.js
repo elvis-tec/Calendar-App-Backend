44a03f330737a2e21296a6d520add6fd5af0ae9e
Elvis Salazar <e2.salazar.acuna.elvis@gmail.com>
2022-10-03 13:32:34 -0600

    Fix event to events

--- controllers/events.js
+++ controllers/events.js
@@ -3,10 +3,10 @@ const Event = require('../models/Event');
 
 const getEvents = async(req, res = response)=> {
     try {
-        const event = await Event.find().populate('user', 'name');
+        const events = await Event.find().populate('user', 'name');
         res.status(200).json({
             ok: true,
-            event,
+            events,
         });
     } catch (error) {
         console.log(error);
